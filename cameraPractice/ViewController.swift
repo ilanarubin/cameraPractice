c1c2145be9febcc07469b28885b2a07e33840dd4
ilanarubin <ilanarubin007@gmail.com>
2020-07-15 12:30:49 -0700

    first commit

--- cameraPractice/ViewController.swift
+++ cameraPractice/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+var imagePicker = UIImagePickerController()
+    
+    
+    @IBOutlet weak var newImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // update our photo with the selected photo
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            newImage.image = selectedImage
+        }
+        // go back to our ViewController so the user can see the update
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func takeASelfie(_ sender: UIButton) {
+        
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated : true, completion: nil)
+    }
+    
+    
+    @IBAction func chooseFromLibrary(_ sender: UIButton) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 
+    
 }
 
